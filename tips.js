6496c82c05bcf42ae2e6780b3a150eac7354c0ce
Destiny Moncrief <D.moncrief4@gmail.com>
2024-04-23 10:34:05 -0400

    made changes

---
+++ tips.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// sample inputs
+
+var foodCost = 79.25;
+
+var tax = 6.54;
+
+var tip = 12.00;
+
+// calculations
+
+var totalDue = 79.25 + 6.54 + 12.00;
+
+// output
+
+console.log("The total due is:" + totalDue);
+
+console.log("Food cost is:" foodCost + " and tax is:" + tax);
+
+console.log("Tip is" + tip);
+
+console.log("Total Due is" + totalDue);
