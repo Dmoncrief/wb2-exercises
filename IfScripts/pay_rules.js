b0431c0a1f3951302a31c2846214a588ae45e2ba
Destiny Moncrief <D.moncrief4@gmail.com>
2024-04-24 15:23:09 -0400

    New exercise

---
+++ IfScripts/pay_rules.js
@@ -0,0 +1,111 @@
+"use strict"
+
+// known values
+ 
+var payRate = 17.30;
+var hoursWorked = 45;
+var grossPay;
+
+
+//  Calculations
+
+var grossPay = 0;
+var overtimeRate = payRate * 1.5;
+
+
+
+
+if (hoursWorked <= 40) {
+
+
+    gorssPay = payRate * hoursWorked;
+}
+else { 
+    grossPay =  (40 * payRate)  + (hoursWorked -40) * payRate * 1.5
+}
+
+
+// Display Calculation
+
+
+var message = "worked" + hoursWorked + " hours for " + grossPay;
+
+console.log(message);
+
+
+// ------------------------------------------------------------------------
+
+
+"use strict"
+
+// known values
+ 
+var payRate = 25.00;
+var hoursWorked = 40;
+var grossPay;
+
+
+//  Calculations
+
+var grossPay = 0;
+var overtimeRate = payRate * 1.5;
+
+
+
+
+if (hoursWorked <= 40) {
+
+
+    grossPay = payRate * hoursWorked;
+}
+else { 
+    grossPay =  (40 * payRate)  + (hoursWorked -40) * payRate * 1.5
+}
+
+
+// Display Calculation
+
+
+var message = "worked" + hoursWorked + " hours for " + grossPay;
+
+console.log(message);
+
+
+// -------------------------------------------------------------------------
+
+
+"use strict"
+
+// known values
+ 
+var payRate = 12.50;
+var hoursWorked = 20;
+var grossPay;
+
+
+//  Calculations
+
+var grossPay = 0;
+var overtimeRate = payRate * 1.5;
+
+
+
+
+if (hoursWorked <= 40) {
+
+
+    grossPay = payRate * hoursWorked;
+}
+else { 
+    grossPay =  (40 * payRate)  + (hoursWorked -40) * payRate * 1.5
+}
+
+
+// Display Calculation
+
+
+var message = "worked" + hoursWorked + " hours for " + grossPay;
+
+console.log(message);
+
+
