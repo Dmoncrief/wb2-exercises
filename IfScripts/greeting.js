846941cd8a5c0ed5a403a3f7271101b70a911f17
Destiny Moncrief <D.moncrief4@gmail.com>
2024-04-25 10:47:56 -0400

    completed exercise

---
+++ IfScripts/greeting.js
@@ -0,0 +1,74 @@
+"use strict"
+
+// Current hour (0-23)
+
+var currentHour = 10;
+
+
+// result of variable
+ var greeting; 
+
+
+
+//  use if statement to set greeting based on current hour
+
+if (currentHour < 10) {
+    greeting = "Good morning!";
+
+} else {
+    greeting = "Hello!";
+}
+
+console.log(greeting);
+
+// ------------------------------------------------------------
+
+
+"use strict"
+
+// Current hour (0-23)
+
+var currentHour = 5;
+
+
+// result of variable
+ var greeting; 
+
+
+
+//  use if statement to set greeting based on current hour
+
+if (currentHour < 5) {
+    greeting = "Good evening!";
+
+} else {
+    greeting = "Good evening!";
+}
+
+console.log(greeting);
+
+// -------------------------------------------------------
+
+"use strict"
+
+// Current hour (0-23)
+
+var currentHour = 12;
+
+
+// result of variable
+ var greeting; 
+
+
+
+//  use if statement to set greeting based on current hour
+
+if (currentHour < 12) {
+    greeting = "Good day!";
+
+} else {
+    greeting = "Good day!";
+}
+
+console.log(greeting);
+
