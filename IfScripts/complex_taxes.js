bc9e369380766eff23fe59aee1b76bb7502fb0db
Destiny Moncrief <D.moncrief4@gmail.com>
2024-04-25 22:55:53 -0400

    completed new exercise

--- IfScripts/complex_taxes.js
+++ IfScripts/complex_taxes.js
@@ -22,12 +22,64 @@ grosspayAnnual = grosspayWeekly * 52;
 
 
 
-if(stat == "single") {
-    if(grosspayAnual <12000)        {taxRatePercent = 0.05; }
-    else if (grosspayAnnual < 2500)
-else if (grosspayAnnual <7500)
-    else /* 75,000 or over*  
-}
+if(stat == "single") 
+    if(grosspayAnnual <12000) {
+        taxRatePercent = 0.05; 
+    } else if (grosspayAnnual < 2500) {
+        taxRatePercent = 0.1;
+    } else if (grosspayAnnual < 7500) {
+        taxRatePercent = 0.15;
+    }  
+    else (grosspayAnnual = 75000) 
+        taxRatePercent = 0.2;
+    
+        // Display results
+
+        console.log("The gross annual pay is:" + grosspayAnnual);
+        console.log("The tax percentage is:" + taxRatePercent);
 
 
-else if
+        // -------------------------------------------------
+
+
+        "use strict"
+
+var payRate = 10;
+var hoursWorked = 45;
+var grosspayWeekly;
+var grosspayAnnual;
+ var stat = "single";
+var taxRatePercent;
+
+
+grosspayWeekly = payRate* hoursWorked
+
+
+// todo actually compute gross pay corretly
+
+grosspayWeekly = 475;
+
+
+
+grosspayAnnual = grosspayWeekly * 52;
+
+
+
+
+if(stat == "joint") 
+    if(grosspayAnnual <12000) {
+        taxRatePercent = 0; 
+    } else if (grosspayAnnual < 2500) {
+        taxRatePercent = 0.6;
+    } else if (grosspayAnnual < 7500) {
+        taxRatePercent = 0.11;
+    }  
+    else (grosspayAnnual = 75000) 
+        taxRatePercent = 0.2;
+    
+         // Display results
+
+         console.log("The gross annual pay is:" + grosspayAnnual);
+         console.log("The tax percentage is:" + taxRatePercent);
+ 
+
