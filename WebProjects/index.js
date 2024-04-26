0d4f31e0740360c57e4e1ae56f9f7826ab5c7622
Destiny Moncrief <D.moncrief4@gmail.com>
2024-04-26 15:32:47 -0400

    made changes

---
+++ WebProjects/index.js
@@ -0,0 +1,39 @@
+"use strict";
+
+window.onload = init;
+
+function init(){
+    const helloBtnElement = document.getElementById("helloBtn");
+    helloBtnElement.onclick = onGreetuserBtnClicked;
+    console.log("index.js init() executed");
+}
+
+
+function onGreetUserBtnClcicked() {
+    alert("Hi there!");
+}
+
+
+const answerElement = document.getElementById("answerField")
+
+
+// create buttons
+
+const addBtn = document.getElementById("addBtn");
+const subtractBtn = document.getElemenyById("subtractBtn");
+const multipyBtn = document.getElementById("multiplyBtn");
+const divideBtn = document.getElemenyById("divideBtn");
+
+addBtn.onclick = onAddButtonClicked;
+subtractBtn.onclick = onSubtractButtonClicked;
+multiplyBtn.onclick = onMultiplyButtonClicked;
+divideBtn.onclick = onDivideButtonClicked;
+
+
+
+function onAddButtonClicked(){
+    const number1Value = number1Element.value;
+    const number2Value = number2Element.value;
+}
+
+console.log(number1Value + )
