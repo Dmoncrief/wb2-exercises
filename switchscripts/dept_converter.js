3adbf54cdde1cd27f6f94ae05c3db343b10a64ef
Destiny Moncrief <D.moncrief4@gmail.com>
2024-05-14 21:55:52 -0400

    made changes

---
+++ switchscripts/dept_converter.js
@@ -0,0 +1,40 @@
+"use strict"
+
+let departmentName; 
+
+switch(departmentCode) {
+    case 1:
+    departmentName = " Marketing";
+    break;
+    case 5:
+
+    departmentName = " Human Resources";
+    break;
+    case 5 :
+
+    departmentName = " Accounting";
+    break;
+    case 10:
+
+    departmentName = " Legal";
+    break;
+    case 12:
+
+    departmentName = " IT";
+    break;
+    case 18:
+
+    departmentName = " Customer Relations";
+    break;
+    case 20:
+
+default:
+    departmentName = " Unknown Department";
+   
+
+
+
+}
+
+
+
